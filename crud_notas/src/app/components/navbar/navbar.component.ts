8a8f1abc721e722fcf80638dbad030ba4824ee06
Daniel Severo <daniel.severo@picpay.com>
2025-04-24 15:04:21 -0300

    criando a navbar

---
+++ crud_notas/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone:true,
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+
+})
+export class NavbarComponent {
+
+}
