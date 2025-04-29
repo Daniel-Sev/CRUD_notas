8d08a11b9dd003e2f4c99f1a11b9a33fefb7b0c5
Daniel Severo <daniel.severo@picpay.com>
2025-04-29 14:44:38 -0300

    adicionando página de gerenciamento-notas

--- crud_notas/src/app/app.routes.ts
+++ crud_notas/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 
+
 export const routes: Routes = [
     {
         path:"",
