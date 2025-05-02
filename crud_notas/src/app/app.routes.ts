85c5eeec5c8c1cae09c10d13ace9aef83ff4f41a
Daniel Severo <daniel.severo@picpay.com>
2025-05-02 11:31:26 -0300

    adicionando página da tabela

--- crud_notas/src/app/app.routes.ts
+++ crud_notas/src/app/app.routes.ts
@@ -3,12 +3,18 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { GraficosComponent } from './components/graficos/graficos.component';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { GerenciamentoNotasComponent } from './components/gerenciamento-notas/gerenciamento-notas.component';
 
 
 export const routes: Routes = [
     {
         path:"",
         component: HomeComponent
+    },
+    {
+        path:"home",
+        component: HomeComponent,
+        redirectTo:""
     },
     {
         path:"login",
@@ -21,5 +27,9 @@ export const routes: Routes = [
     {
         path:"cadastro",
         component: CadastroComponent
+    },
+    {
+        path:"gerenciamento",
+        component: GerenciamentoNotasComponent
     }
 ];
---
+++ crud_notas/src/app/components/gerenciamento-notas/gerenciamento-notas.component.ts
@@ -0,0 +1,201 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { retry } from 'rxjs';
+
+@Component({
+  selector: 'app-gerenciamento-notas',
+  standalone: true,
+  imports: [CommonModule, NavbarComponent, SidebarComponent],
+  templateUrl: './gerenciamento-notas.component.html',
+  styleUrls: ['./gerenciamento-notas.component.css']
+})
+
+export class GerenciamentoNotasComponent {
+  
+  pagina_atual: number = 1;
+  itens_pagina: number = 5;
+  
+  itens: any = [
+    {
+      nome: "Daniel",
+      email: "daniel@gmail.com",
+      ano: 2,
+      serie: "G",
+      exatas: 10,
+      linguagens: 10,
+      ciencias: 10,
+      media: 10
+    },
+    {
+      nome: "Mariana",
+      email: "mariana@gmail.com",
+      ano: 1,
+      serie: "B",
+      exatas: 9,
+      linguagens: 8,
+      ciencias: 9,
+      media: 2  
+      
+    },
+    {
+      nome: "Lucas",
+      email: "lucas@gmail.com",
+      ano: 3,
+      serie: "A",
+      exatas: 7,
+      linguagens: 6,
+      ciencias: 8,
+      media: 7
+    },
+    {
+      nome: "Beatriz",
+      email: "beatriz@gmail.com",
+      ano: 2,
+      serie: "D",
+      exatas: 10,
+      linguagens: 9,
+      ciencias: 10,
+      media: 9.7
+    },
+    {
+      nome: "Felipe",
+      email: "felipe@gmail.com",
+      ano: 1,
+      serie: "E",
+      exatas: 6,
+      linguagens: 7,
+      ciencias: 5,
+      media: 6
+    },
+    {
+      nome: "Ana Clara",
+      email: "ana.clara@gmail.com",
+      ano: 3,
+      serie: "C",
+      exatas: 8,
+      linguagens: 9,
+      ciencias: 8,
+      media: 8.3
+    },
+    {
+      nome: "Pedro",
+      email: "pedro@gmail.com",
+      ano: 2,
+      serie: "F",
+      exatas: 7,
+      linguagens: 7,
+      ciencias: 6,
+      media: 6.7
+    },
+    {
+      nome: "Camila",
+      email: "camila@gmail.com",
+      ano: 1,
+      serie: "A",
+      exatas: 9,
+      linguagens: 9,
+      ciencias: 8,
+      media: 8.7
+    },
+    {
+      nome: "Rafael",
+      email: "rafael@gmail.com",
+      ano: 3,
+      serie: "B",
+      exatas: 5,
+      linguagens: 6,
+      ciencias: 7,
+      media: 6
+    },
+    {
+      nome: "Isabela",
+      email: "isabela@gmail.com",
+      ano: 2,
+      serie: "E",
+      exatas: 8,
+      linguagens: 8,
+      ciencias: 9,
+      media: 8.3
+    },
+    {
+      nome: "Gustavo",
+      email: "gustavo@gmail.com",
+      ano: 1,
+      serie: "D",
+      exatas: 6,
+      linguagens: 5,
+      ciencias: 7,
+      media: 6
+    },
+    {
+      nome: "Laura",
+      email: "laura@gmail.com",
+      ano: 3,
+      serie: "C",
+      exatas: 10,
+      linguagens: 9,
+      ciencias: 9,
+      media: 9.3
+    },
+    {
+      nome: "Thiago",
+      email: "thiago@gmail.com",
+      ano: 2,
+      serie: "F",
+      exatas: 7,
+      linguagens: 8,
+      ciencias: 7,
+      media: 7.3
+    }
+  ];
+  itens_listados: any[] = [];
+  total_pagina: any[] = []
+  constructor() {
+    this.atualizarItensListados();
+    this.total_pagina = this.calcularPaginas()
+  }
+  
+  calcularPaginas(){
+    let total_pagina: any[] = []
+    for (let index = 0; index < Math.ceil(this.itens.length/this.itens_pagina); index++) {
+     total_pagina.push(index+1)
+    }
+    return total_pagina
+  }
+  
+ 
+
+  atualizarItensListados() {
+    const indice_inicio = (this.pagina_atual - 1) * this.itens_pagina;
+    const indice_fim = indice_inicio + this.itens_pagina;
+    this.itens_listados = this.itens.slice(indice_inicio, indice_fim);
+  }
+
+  paginaAnterior() {
+    if (this.pagina_atual > 1) {
+      this.pagina_atual-=1;
+      this.atualizarItensListados();
+    }else{
+      this.proximaPagina()
+    }
+  }
+
+  proximaPagina() {
+    if ((this.pagina_atual * this.itens_pagina) < this.itens.length) {
+      this.pagina_atual++;
+      this.atualizarItensListados();
+    }else{
+      this.paginaAnterior()
+    }
+  }
+  clique(){
+    console.log("é.....")
+  }
+  navegarPara(pagina: number){
+    this.pagina_atual = pagina
+    console.log("Deu certo")
+    this.atualizarItensListados()
+  }
+}
