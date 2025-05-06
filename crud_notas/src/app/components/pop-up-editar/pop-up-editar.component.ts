6c42b651e6a728b4abebd15eac7a16fc90cbb415
Daniel Severo <daniel.severo@picpay.com>
2025-05-06 18:16:31 -0300

    terminando o projeto

---
+++ crud_notas/src/app/components/pop-up-editar/pop-up-editar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pop-up-editar',
+  imports: [],
+  templateUrl: './pop-up-editar.component.html',
+  styleUrl: './pop-up-editar.component.css'
+})
+export class PopUpEditarComponent {
+
+}
